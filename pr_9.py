2c1fdb29b3aa91223edd9ae78c33e0306559049e
mrudulakulkarni791 <mrudulakulkarni34@gmail.com>
2026-05-07 20:25:38 +0530

    Update pr_9.py

--- pr_9.py
+++ pr_9.py
@@ -1,3 +1,12 @@
+9
+Data Visualization II: On Titanic dataset
+1. Plot a box plot for distribution of age with
+respect to each gender along with the
+information about whether they survived or
+not. (Column names : 'sex' and 'age')
+2. Write observations on the inference from
+the above statistics.
+
 #9th
 import pandas as pd
 import seaborn as sns
