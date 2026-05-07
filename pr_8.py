1fcc33067f83dcc255c2d6f3ab1f658a7715487f
mrudulakulkarni791 <mrudulakulkarni34@gmail.com>
2026-05-07 20:25:09 +0530

    Update pr_8.py

--- pr_8.py
+++ pr_8.py
@@ -1,3 +1,16 @@
+8
+Data Visualization I: 1. Use the inbuilt
+dataset 'titanic'. The dataset contains 891
+rows and contains information about
+the passengers who boarded the
+unfortunate Titanic ship. Use the Seaborn
+library to see if we can find any patterns in
+the data.
+2. Write a code to check how the price of
+the ticket (column name: 'fare') for each
+passenger is distributed by plotting a
+histogram.
+
 #8th
 import seaborn as sns
 import matplotlib.pyplot as plt
